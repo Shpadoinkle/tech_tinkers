ae53cc3d0b0c9120f4f112320ad0c75425bc6434
Shpadoinkle <ash.3davis@gmail.com>
2022-06-23 19:33:13 +1000

    changes

--- 2019/lottoNumberPicker/src/App.js
+++ 2019/lottoNumberPicker/src/App.js
@@ -16,13 +16,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     var config = {
-      apiKey: "AIzaSyDyUuuEHZA8WfuC5PhwVftvw9fUfENa5tM",
-      authDomain: "thoash-7b778.firebaseapp.com",
-      databaseURL: "https://thoash-7b778.firebaseio.com",
-      projectId: "thoash-7b778",
-      storageBucket: "thoash-7b778.appspot.com",
-      messagingSenderId: "213870389423",
-      appId: "1:213870389423:web:f0799434da8eb1ed"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
     };
     firebase.initializeApp(config);
   }
--- 2019/movieApi/src/App.js
+++ 2019/movieApi/src/App.js
@@ -17,13 +17,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     var config = {
-      apiKey: "AIzaSyDyUuuEHZA8WfuC5PhwVftvw9fUfENa5tM",
-      authDomain: "thoash-7b778.firebaseapp.com",
-      databaseURL: "https://thoash-7b778.firebaseio.com",
-      projectId: "thoash-7b778",
-      storageBucket: "thoash-7b778.appspot.com",
-      messagingSenderId: "213870389423",
-      appId: "1:213870389423:web:b75405ac6dab3002"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
     };
     firebase.initializeApp(config);
   }
