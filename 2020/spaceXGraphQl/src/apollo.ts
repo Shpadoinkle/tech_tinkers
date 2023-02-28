3f0970f0f35e7101db2b609b766274c55288e37a
Shpadoinkle <ash.3davis@gmail.com>
2023-02-28 21:26:19 +1000

    update spacex graph uri

--- 2020/spaceXGraphQl/src/apollo.ts
+++ 2020/spaceXGraphQl/src/apollo.ts
@@ -1,7 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://api.spacex.land/graphql/",
+  /**
+   * Original graphql has shut down.  Using clones
+   */
+  // uri: "https://api.spacex.land/graphql/",
+  uri: "https://spacex-production.up.railway.app/",
   cache: new InMemoryCache(),
 });
 
